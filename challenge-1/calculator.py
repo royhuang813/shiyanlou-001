011735f42b4860e93d07a98bda99e0fe0e16426a
626732786 <626732786@qq.com>
2017-10-21 16:57:15 +0800

    function

--- challenge-1/calculator.py
+++ challenge-1/calculator.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-import sys
+def f(x):
+    b = x - 3500
+    if b <= 0:
+        print(format(0,'.2f'))
+    elif 0 < b <= 1500:
+        print(format(b * 0.03 - 0, '.2f'))
+    elif 1500 < b <= 4500:
+        print(format(b * 0.1 - 105, '.2f'))
+    elif 4500 < b <= 9000:
+        print(format(b * 0.2 - 555, '.2f'))
+    elif 9000 < b <= 35000:
+        print(format(b * 0.25 - 1005, '.2f'))
+    elif 35000 < b <= 55000:
+        print(format(b * 0.3 - 2755, '.2f'))
+    elif 55000 < b <= 80000:
+        print(format(b * 0.35 - 5505, '.2f'))
+    else:
+        print(format(b * 0.45 - 13505, '.2f'))
+if __name__ == '__main__':
+    import sys
 try:
-    a = int(sys.argv[1])
+    salary = int(sys.argv[1])
+    f(salary)
 except ValueError:
-    print("Parameter Error")
-b = a - 3500
-if b <= 0:
-    print(format(0 , ".2f"))
-if 0 < b <= 1500:
-    print(format(b * 0.03 - 0 , ".2f"))
-if 1500 < b <= 4500:
-    print(format(b * 0.1 - 105 , ".2f"))
-if 4500 < b <= 9000:
-    print(format(b * 0.2 - 555 , ".2f"))
-if 9000 < b <= 35000:
-    print(format(b * 0.25 - 1005 , ".2f"))
-if 3500 < b <= 55000:
-    print(format(b * 0.3 - 2755 , ".2f"))
-if 55000 < b <= 80000:
-    print(format(b * 0.35 - 5505 , ".2f"))
-if b > 80000:
-    print(format(b * 0.45 - 13505 , ".2f"))
-
+    print('Parameter Error')
